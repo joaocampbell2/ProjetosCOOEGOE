4a267e7a0eb125c49b6e8498c48a37a65c9dd9d3
joaocampbell <jpcampbell2012@hotmail.com>
2024-09-05 15:07:21 -0300

    feat: verificar competencia

--- Execução Fiscal - COAJUR/verificarProcesso.py
+++ Execução Fiscal - COAJUR/verificarProcesso.py
@@ -16,6 +16,7 @@ from shutil import move
 import tabula
 from PyPDF2 import PdfReader
 
+
 def loginSEI():
     navegador.get("https://sei.rj.gov.br/sip/login.php?sigla_orgao_sistema=ERJ&sigla_sistema=SEI")
 
@@ -41,15 +42,53 @@ def loginSEI():
 
 def trocarCoordenacao():
     coordenacao = navegador.find_elements(By.XPATH, "//a[@id = 'lnkInfraUnidade']")[1]
-    print(coordenacao)
     if coordenacao.get_attribute("innerHTML") == 'SEFAZ/COOEGOE':
-        print(coordenacao)
         coordenacao.click()
         WebDriverWait(navegador,5).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Trocar Unidade')]")))
         navegador.find_element(By.XPATH, "//td[text() = 'SEFAZ/COOAJUR' ]").click() 
-    
-navegador = webdriver.Firefox()
+        
+def abrirPastas():
+    listaDocs = WebDriverWait(navegador,5).until(EC.presence_of_element_located((By.ID, "divArvore")))
+    pastas = listaDocs.find_elements(By.XPATH, '//a[contains(@id, "joinPASTA")]')
+    for doc in pastas[:-1]:
+        doc.click() 
+        sleep(4)
+        
+def verificarCompetencia():
+    docs = navegador.find_elements(By.XPATH, "//div[@id = 'divArvore']//div//a[@class = 'infraArvoreNo']")
+    quantDocs = len(docs) 
+    for doc in reversed(range(quantDocs)):
+            docTexto = docs[doc].text
+            if "Despacho de Encaminhamento de Processo" in docTexto:
+                docs[doc].click()
+                navegador.switch_to.default_content()            
+                WebDriverWait(navegador,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrVisualizacao")))
+                WebDriverWait(navegador,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrArvoreHtml")))
+                body = navegador.find_element(By.TAG_NAME, 'body').text
+                if "Conforme despacho retro, solicitamos o processamento do DARJ ora acostado." in body:
+                    if "AUDITORA FISCAL" in body.upper() or "AUDITOR FISCAL" in body.upper():
+                        return True
+                    else:
+                        return False
+                        
+    return False
 
+    
+                
+navegador = webdriver.Firefox()
+processoSEI = "SEI-140011/000214/2022"
 
 loginSEI()
+
+barraPesquisa = navegador.find_element(By.ID, "txtPesquisaRapida")
+
+barraPesquisa.send_keys(processoSEI)
+barraPesquisa.send_keys(Keys.ENTER)
+
+WebDriverWait(navegador,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrArvore")))
+
+abrirPastas()
+
+
+
 #navegador.quit()
