2b6e8b93cad6b1de0fcd0ebcef7ad69e1402eade
joaocampbell <jpcampbell2012@hotmail.com>
2024-09-24 16:22:56 -0300

    feat: bloco e anotação

--- Execução Fiscal - COAJUR/verificarProcesso.py
+++ Execução Fiscal - COAJUR/verificarProcesso.py
@@ -71,18 +71,18 @@ def verificarCompetencia():
                 WebDriverWait(navegador,5).until(EC.presence_of_element_located((By.XPATH, '//p[contains(text(), "Rio de Janeiro")]')))
 
                 body = navegador.find_element(By.TAG_NAME, 'body').text
-                if "processamento do DARJ" in body:
+                if "Para o processamento do DARJ" in body or "solicitamos o processamento do DARJ" in body or "Para processamento do DARJ" in body:
                     if "AUDITORA FISCAL" in body.upper() or "AUDITOR FISCAL" in body.upper():
-                        return "ok"
+                        return "Ok"
                     else:
-                        return "inválida"
+                        return "Inválida"
                 else:
                     navegador.switch_to.default_content()
                     WebDriverWait(navegador,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrArvore")))
                     docs = navegador.find_elements(By.XPATH, "//div[@id = 'divArvore']//div//a[@class = 'infraArvoreNo']")
             
                         
-    return "inválida"
+    return "Não encontrada"
 
 
 def preencherPlanilha():
@@ -111,14 +111,15 @@ def preencherPlanilha():
     processoJudicial = None #1 OU SEGUNDO DOCUMENTO
 
 def verificarValorEValidade():
+    montanteDARJ = None
+
     navegador.switch_to.default_content()
     WebDriverWait(navegador,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrArvore")))
-
     docs = navegador.find_elements(By.XPATH, "//div[@id = 'divArvore']//div//a[@class = 'infraArvoreNo']")
     quantDocs = len(docs) 
     for doc in reversed(range(quantDocs)):
         docTexto = docs[doc].text
-        if "DARJ CDA" in docTexto:
+        if "DARJ" in docTexto:
             docs[doc].click()
             navegador.switch_to.default_content()            
             WebDriverWait(navegador,20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ifrVisualizacao")))
@@ -132,9 +133,9 @@ def verificarValorEValidade():
 
             dias = (validadeData - datetime.now()).days
             if dias < 0:
-                validade = "Guia fora de validade"
+                validade = "Fora de validade"
             else:
-                validade = "ok"
+                validade = "Ok"
             montanteDARJ = re.search(r"(TOTAL A PAGAR)\n\n([\n*\w*\s\(\)\.,-]*)\n\n14 ",body).group(2)  #DARJ
             
             navegador.switch_to.default_content()
@@ -155,10 +156,13 @@ def verificarValorEValidade():
             montanteGuia = re.search(r"Não\n.?(\d[\n*\w*\s\(\)\.,-\/]*,\d\d)\n\nU", body).group(1)
             break      
     
+    if montanteDARJ == None:
+        return "DARJ não encontrado", "DARJ não encontrado"
+
     if montanteDARJ != montanteGuia:
         montante  = "Montante Guia diferente de Montante DARJ"            
     else:
-        montante = "ok"
+        montante = "Ok"
         
     return validade, montante
    
@@ -173,14 +177,50 @@ def acessarBloco():
         if nBloco.text == blocoSolicitado:
             nBloco.find_element(By.XPATH, './/a').click()
             break
-                  
+
+def anotarInformacoes():
+    print(f"======================={i}========================")
+
+    print("Validade DARJ: " + validade)
+    print("Montante: " + montante)
+    print("Competência: " + competencia)
+    
+    processo.find_element(By.XPATH,".//td//a//img[@title='Anotações']").click()
+                        
+    time.sleep(2)
+    try:
+        iframe = navegador.find_element(By.TAG_NAME, 'iframe')
+        navegador.switch_to.frame(iframe)
+
+        txtarea = navegador.find_element(By.XPATH, '//textarea[@id = "txtAnotacao"]')
+
+        txtarea.send_keys(Keys.PAGE_DOWN)
+        txtarea.send_keys(Keys.END)
+        txtarea.send_keys(Keys.ENTER)
+        txtarea.send_keys("Validade DARJ: " + validade)
+        txtarea.send_keys(Keys.ENTER)
+        txtarea.send_keys("Montante: " + montante)
+        txtarea.send_keys(Keys.ENTER)
+        txtarea.send_keys("Competência: " + competencia)
+        txtarea.send_keys(Keys.ENTER)
+        
+        time.sleep(1)
+        salvar = navegador.find_element(By.XPATH, '//button[@value = "Salvar"]')
+
+        salvar.click()
+        
+    except:
+       traceback.print_exc()
+       time.sleep(1)
+       navegador.find_element(By.XPATH, "//div[@class = 'sparkling-modal-close']").click()
+    finally:
+        navegador.switch_to.default_content()
+
 navegador = webdriver.Firefox()
 
 blocoSolicitado = "616986"
-processoSEI = "SEI-140001/050926/2024"
 
 loginSEI()
-
 
 acessarBloco()
 
@@ -191,25 +231,29 @@ for i in range(1,len(processos)):
     WebDriverWait(navegador,20).until(EC.invisibility_of_element_located(((By.XPATH, "//div[@class = 'sparkling-modal-close']"))))
     WebDriverWait(navegador,20).until(EC.presence_of_element_located(((By.XPATH, "//tbody//tr"))))
     processo = navegador.find_elements(By.XPATH, "//tbody//tr")[i]
+    print(processo.text)
+    
     nProcesso = processo.find_element(By.XPATH, './/td[3]//a').text
 
-    WebDriverWait(processo,20).until(EC.element_to_be_clickable(((By.XPATH, './/td[3]//a')))).click()
+    if "Montante" not in processo.text and "Validade" not in processo.text and "Competencia" not in processo.text:
 
-    time.sleep(3)  
+        WebDriverWait(processo,20).until(EC.element_to_be_clickable(((By.XPATH, './/td[3]//a')))).click()
 
-    navegador.switch_to.window(navegador.window_handles[1])
-    
-    abrirPastas()
+        time.sleep(3)  
 
-    validade, montante = verificarValorEValidade()
+        navegador.switch_to.window(navegador.window_handles[1])
+        
+        abrirPastas()
 
-    competencia = verificarCompetencia()
+        validade, montante = verificarValorEValidade()
 
-    print("Validade: " + validade)
-    print("Montante: " + montante)
-    print("Competencia: " + competencia)
-    print(f"======================={i}========================")
-    navegador.close()
-    navegador.switch_to.window(navegador.window_handles[0])
+        competencia = verificarCompetencia()
+
+        
+        navegador.close()
+        navegador.switch_to.window(navegador.window_handles[0])
+        
+        anotarInformacoes()
+
 
 #navegador.quit()
