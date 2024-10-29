92363161c4924c70a1694970a271e7a0fc205ebd
joaocampbell <jpcampbell2012@hotmail.com>
2024-10-29 12:04:03 -0300

    feat: detectar elementos execução fiscal

--- Fiança -  COOEGOE/FazerAcompanhamentoEspecial.py
+++ Fiança -  COOEGOE/FazerAcompanhamentoEspecial.py
@@ -11,7 +11,7 @@ import os
 import pandas as pd
 
 from tqdm import tqdm
-from marinetteSEFAZ import loginSEI, salvarPlanilha, obterProcessosDeBloco, procurarArquivos, buscarInformacaoEmDocumento
+from marinetteSEFAZ import loginSEI, salvarPlanilha, obterProcessosDeBloco, procurarArquivos, buscarInformacaoEmDocumento,buscarProcessoEmBloco
 
 bloco = input("Digite o número do bloco: ")
     
@@ -26,29 +26,62 @@ match grupo:
         grupo = "CAUÇÃO"
     case _:
         ""
-marinette = r"C:\Users\jcampbell1\OneDrive - SEFAZ-RJ\CONTROLE GERENCIAL - PAGAMENTOS\Planilha Gerencial - Marinette.xlsx"
-df = pd.read_excel(marinette, sheet_name=bloco)
-
-
-navegador = webdriver.Firefox()
-loginSEI(navegador,os.environ['login_sefaz'],os.environ['senha_sefaz'],"SEFAZ/COOEGOE")
-processos = obterProcessosDeBloco(navegador,bloco)
-
-for processo in tqdm(processos, total = len(processos)):
-    
-    WebDriverWait(navegador,20).until(EC.invisibility_of_element_located(((By.XPATH, "//div[@class = 'sparkling-modal-close']"))))
-    WebDriverWait(navegador,20).until(EC.presence_of_element_located(((By.XPATH, "//tbody//tr"))))
-    nProcesso = processo.find_element(By.XPATH, './/td[3]//a').text
-    
-    
-    if df.loc[df[df["PROCESSO"] == nProcesso].index[0], "ACOMPANHAMENTO ESPECIAL"] != "Ok":
-        WebDriverWait(processo,20).until(EC.element_to_be_clickable(((By.XPATH, './/td[3]//a')))).click()
-        navegador.switch_to.window(navegador.window_handles[1])
         
+nav = webdriver.Firefox()
+loginSEI(nav,os.environ['login_sefaz'],os.environ['senha_sefaz'],"SEFAZ/COOEGOE")
+processos = obterProcessosDeBloco(nav,bloco)
+
+for i in tqdm(range(1,len(processos[1:]) + 1)):
+    #try:
+        processo = nav.find_elements(By.XPATH, "//tbody//tr")[i]
+
+    
+        linkProcesso = buscarProcessoEmBloco(nav,i)
         
-        if grupo == "FIANÇA E VALOR APREENDIDO":
-            docs = procurarArquivos(navegador, "Despacho sobre Autorização de Despesa")
-            for doc in reversed(docs):
-                buscarInformacaoEmDocumento(navegador,doc,)
+        nProcesso = linkProcesso.text
+    
+        if "Acompanhamento Especial preenchido" not in processo.text:
+            linkProcesso.click()
+            nav.switch_to.window(nav.window_handles[1])
+            
+            
+            valoresExtra = []
+            valoresOrcamentario = []
+            beneficiarios = []
+            pagamentos = []
+            
+            
+            despachos = procurarArquivos(nav, "Despacho sobre Autorização de Despesa")
+
+            
+            regexFiança = ""
+            
+            
+            if grupo == "FIANÇA E VALOR APREENDIDO":
+                for doc in reversed(despachos):
+                    buscarInformacaoEmDocumento(nav,doc,"",)
+                    
+                    #Buscar oq??
+            if grupo == "EXECUÇÃO FISCAL":
+                darjs = procurarArquivos(nav, "DARJ")
+                   
+                regexExecucao = r"(CDA \d{4}\/\d{3}\.\d{3}\-\d ?|CDA \d*)[\s\S]*?no valor de R\$ ([^(]*)"
+                resultado = buscarInformacaoEmDocumento(nav,despachos[-1],regexExecucao,"Coordenador")
+                
+                cda = resultado.group(1)
+                valor = resultado.group(2)
+                    
+                regexDARJ = r"\bNOME\n?\n?([\n*\w*\s\(\)\.,-]*)08 - CNPJ\/CPF\n?\n?([\n\d\.\-\/]*)02 - ENDEREÇO COMPLETO"
+                
+                resultado = buscarInformacaoEmDocumento(nav,darjs[-1],regexDARJ,"ESTADO")
+                
+                nome = resultado.group(1).strip()
+                cpf = resultado.group(2).strip()
+                
+                texto = [cda + " /","Nome:" + nome + " /", "CPF-CNPJ: " + cpf + " /", "Valor: R$" + valor + " /"]
+                                
+            if grupo == "CAUÇÃO":
+                pass
+                
+    #except:
         
-           
